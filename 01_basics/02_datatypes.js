70aed09b7d6944e7dd5e140fed29fe8081b70492
Sayandas8466 <103281376+Sayandas8466@users.noreply.github.com>
2024-01-31 07:08:23 +0000

    Data_types done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS Code as newer version
+
+// alert(3*3) // we are using node js , not a browser
+
+//Readability is more important
+
+let name = "Sayan" //String allocation
+let age = 23 // number allocation
+let isLoggedIn = true //boolean => true/false
+
+//bigInt
+//null => standalone value 
+//undefined => not assigning any value 
+//symbol => unique
+
+//object 
+
+
+console.log(typeof(age)); // what is the type of variable
+console.log(typeof(null)); // type of null is object
