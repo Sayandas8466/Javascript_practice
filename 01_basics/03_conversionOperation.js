c7334f46b257fb58426745f0f64f53c0690503d4
Sayandas8466 <103281376+Sayandas8466@users.noreply.github.com>
2024-02-02 07:32:31 +0000

    Dtatatypes Summary

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 33 
+
+  console.log(typeof score);
+  console.log(typeof(score));
+
+let valueInString = String(score);
+
+console.log(typeof valueInString);
+
+// 33 => 33
+//33abc =>NaN
+//true =>1 ; false => 0
+
+let isLoggedIn = "sayan "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
---
+++ 01_basics/04_DatatypesSummary.js
@@ -0,0 +1,9 @@
+  // Primitive
+
+  // 7 types : Strings , Number , Boolean, null, undefined , Symbol, BigInt
+
+
+
+  // Reference  (Non-Primitive)
+
+  // Array , objects , Functions
