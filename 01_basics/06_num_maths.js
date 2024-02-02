4cf7027b0e7905705833df0c001e733886c6ad1a
Sayandas8466 <103281376+Sayandas8466@users.noreply.github.com>
2024-02-02 09:58:30 +0000

    Date-time-maths

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100);
+
+console.log(balance)
+console.log(balance.toString().length);
+
+const othNumbers = 123.8964
+
+// console.log(othNumbers.toPrecision(4));
+
+// const hundereds = 100000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// ============== Maths  ===================
+
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.8));
+
+console.log(Math.floor(Math.random()*10));
+
+console.log(Math.random(10,20));
